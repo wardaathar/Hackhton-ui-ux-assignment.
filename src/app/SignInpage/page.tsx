7bbbd46aae70c8bfe8afc252fb3325a23e72e7d0
wardaathar <wardaathar1234@gmail.com>
2024-12-08 10:53:46 -0700

    first commit

--- src/app/SignInpage/page.tsx
+++ src/app/SignInpage/page.tsx
@@ -13,7 +13,7 @@ export default function SignInPage() {
       {/* Background Image */}
       <div
         className="h-[300px] md:h-[400px] lg:h-[500px] bg-cover bg-center"
-        style={{ backgroundImage: "url('assets/header.svg')" }}>
+        style={{ backgroundImage: "url('assets/rect.png')" }}>
     
         {/* Navbar */}
         <Navbar />
