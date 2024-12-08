47952b625720f643fa6a2a9f396ef790995f35ae
wardaathar <wardaathar1234@gmail.com>
2024-12-08 08:48:09 -0700

    first commit

---
+++ src/app/Ourchef/page.tsx
@@ -0,0 +1,37 @@
+
+
+import React from "react";
+
+import Image from "next/image";
+
+import chef1 from "../assets/Chef Card 1.png";
+import chef2 from "../assets/Card 2.png";
+import chef3 from "../assets/Card 3.png";
+import chef4 from "../assets/Card 4.png";
+
+ 
+function Ourchef() {
+  return (
+    <>
+      <section className="bg-black md:px-[135px] py-[50px]">
+
+        <div className="flex flex-col justify-center items-center">
+      <h1 className="md:text-[32px] text-[24px] font-normal text-[#FF9F0D] font whitespace-nowrap font-greatVibes">
+      Chefs
+        </h1>
+        <h1 className="text-[20px] text-white md:text-[50px] font-bold whitespace-nowrap md:whitespace-normal">
+          <span className="text-[#FF9F0D]">Me</span>et Our Chef
+        </h1>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center gap-[20px] mt-[50px]">
+          <Image src={chef1} alt="" className="w-[200px] md:w-[305px] md:h-[280px] cursor-pointer"/>
+          <Image src={chef2} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
+          <Image src={chef3} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
+          <Image src={chef4} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Ourchef;
---
+++ src/app/Shoplist/page.tsx
@@ -0,0 +1,64 @@
+
+
+import React from 'react';
+import Image from "next/image";
+import menu from "../assets/menu.png";
+
+const menuItems = [
+  { name: 'Lettuce Leaf', description: 'Lacus nisl, et ac dapibus velit in consequat.', price: '12.5$', image: '/menu1.png' },
+  { name: 'Fresh Breakfast', description: 'Lacus nisl, et ac dapibus velit in consequat.', price: '14.5$', image: '/menu2.png' },
+  { name: 'Mild Butter', description: 'Lacus nisl, et ac dapibus velit in consequat.', price: '12.5$', image: '/menu3.png' },
+  { name: 'Fresh Bread', description: 'Lacus nisl, et ac dapibus velit in consequat.', price: '12.5$', image: '/menu4.png' },
+  { name: 'Glow Cheese', description: 'Lacus nisl, et ac dapibus velit in consequat.', price: '12.5$', image: '/menu5.png' },
+  { name: 'Italian Pizza', description: 'Lacus nisl, et ac dapibus velit in consequat.', price: '14.5$', image: '/menu6.png' },
+  { name: 'Slice Beef', description: 'Lacus nisl, et ac dapibus velit in consequat.', price: '12.5$', image: '/menu7.png' },
+  { name: 'Mushroom Pizza', description: 'Lacus nisl, et ac dapibus velit in consequat.', price: '12.5$', image: '/menu8.png' },
+];
+
+const MenuCom = () => {
+  return (
+    <section className='bg-black px-8 sm:px-16 lg:px-[135px] text-white py-12'>
+      <div className="container mx-auto">
+        <h2 className="text-2xl font-bold text-center mb-2">Choose & Pick</h2>
+        <h1 className="text-3xl font-bold text-center mb-8">From Our Menu</h1>
+
+        <div className="flex justify-center mb-8">
+          <ul className="flex flex-wrap justify-evenly space-x-2 sm:space-x-4">
+            <li className="cursor-pointer hover:text-yellow-500">Breakfast</li>
+            <li className="cursor-pointer hover:text-yellow-500">Lunch</li>
+            <li className="cursor-pointer hover:text-yellow-500">Dinner</li>
+            <li className="cursor-pointer hover:text-yellow-500">Dessert</li>
+            <li className="cursor-pointer hover:text-yellow-500">Drink</li>
+            <li className="cursor-pointer hover:text-yellow-500">Snack</li>
+            <li className="cursor-pointer hover:text-yellow-500">Soups</li>
+          </ul>
+        </div>
+
+        <div className='flex flex-col lg:flex-row'>
+          {/* Left Section: Decorative Image */}
+          <div className="hidden lg:block w-1/3 md:w-[50%]">
+            <Image src={menu} alt="Menu Decor" layout="responsive" />
+          </div>
+
+          {/* Right Section: Menu Items */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 flex-grow">
+            {menuItems.map((item, index) => (
+              <div key={index} className="flex 00 rounded-lg overflow-hidden shadow-lg">
+                <div className="w-1/3 ">
+                  <Image src={item.image} alt={item.name} width={100} height={100} className="object-cover" />
+                </div>
+                <div className="p-4 w-2/3">
+                  <h1 className="text-lg font-semibold">{item.name}</h1>
+                  <p className="text-[11px] text-gray-400 mb-2">{item.description}</p>
+                  <h2 className="text-yellow-400 font-bold">{item.price}</h2>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MenuCom;
