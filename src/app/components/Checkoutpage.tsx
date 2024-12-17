065a20d1a577cb174e53840a31c6d1cf03e4c860
wardaathar <wardaathar1234@gmail.com>
2024-12-17 09:22:01 -0700

    first commited

---
+++ src/app/components/Checkoutpage.tsx
@@ -0,0 +1,11 @@
+import Checkout from "./Checkoutt"; // Adjust the path as necessary
+
+const CheckoutPage = () => {
+  return (
+    <div className="p-4">
+      <Checkout /> {/* Render the Checkout component */}
+    </div>
+  );
+};
+
+export default CheckoutPage;
---
+++ src/app/components/Checkoutt.tsx
@@ -0,0 +1,12 @@
+const Checkoutt = () => {
+    return (
+      <div className="p-4">
+        <h3 className="text-xl font-bold">Checkout</h3>
+        <p>Thank you for your purchase! Your order is being processed.</p>
+        {/* You can add more checkout details here */}
+      </div>
+    );
+  };
+  
+  export default Checkoutt;
+  
