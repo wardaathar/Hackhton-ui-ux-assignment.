7a32379fcb2a8d6c836248e54050be9b6a27f4d7
wardaathar <wardaathar1234@gmail.com>
2024-12-20 04:23:11 -0700

    4th commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import Hero from "./Hero"
 import React from "react"
 import Image from "next/image"
-import google from "../public/google.png";
-import apple from "../public/apple.png";
+import google from "../../../public/google.png";
+import apple from "../../../public/Apple.png";
 
 export default function SignUpPage() {
   return (
